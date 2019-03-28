abc7c8a95564a920c9e36f1d9f61d3aeb1f3261b
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-03-29 00:10:50 +0300

    Add files via upload

---
+++ SayisalLoto/SayisalLoto/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace SayisalLoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.btnOyna = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(20, 18);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(54, 57);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "0";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl2.Location = new System.Drawing.Point(92, 18);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(54, 57);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "0";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl3.Location = new System.Drawing.Point(166, 18);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(54, 57);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "0";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl4.Location = new System.Drawing.Point(242, 18);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(54, 57);
+            this.lbl4.TabIndex = 3;
+            this.lbl4.Text = "0";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl5.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl5.Location = new System.Drawing.Point(321, 18);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(54, 57);
+            this.lbl5.TabIndex = 4;
+            this.lbl5.Text = "0";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl6.Location = new System.Drawing.Point(395, 18);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(54, 57);
+            this.lbl6.TabIndex = 5;
+            this.lbl6.Text = "0";
+            // 
+            // btnOyna
+            // 
+            this.btnOyna.Location = new System.Drawing.Point(64, 96);
+            this.btnOyna.Name = "btnOyna";
+            this.btnOyna.Size = new System.Drawing.Size(354, 46);
+            this.btnOyna.TabIndex = 6;
+            this.btnOyna.Text = "Oyna";
+            this.btnOyna.UseVisualStyleBackColor = true;
+            this.btnOyna.Click += new System.EventHandler(this.btnOyna_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 154);
+            this.Controls.Add(this.btnOyna);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Button btnOyna;
+    }
+}
+
---
+++ SayisalLoto/SayisalLoto/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayisalLoto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOyna_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            lbl1.Text = random.Next(0,47).ToString();
+            lbl2.Text = random.Next(0, 47).ToString();
+            lbl3.Text = random.Next(0, 47).ToString();
+            lbl4.Text = random.Next(0, 47).ToString();
+            lbl5.Text = random.Next(0, 47).ToString();
+            lbl6.Text = random.Next(0, 47).ToString();
+        }
+    }
+}
